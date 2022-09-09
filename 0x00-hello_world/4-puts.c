da692a0c2fad887fa67d9d9592fa963c7f3b36ff
Kuddahjames <kudayesani2@gmail.com>
2022-09-09 14:41:31 -0700

    my task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - entering point
+*
+*return: 0 after printing the put function
+*/
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
