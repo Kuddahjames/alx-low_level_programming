75fb80854f7ce73c290f23728e5fb8f99688c277
Kuddahjames <kudayesani2@gmail.com>
2022-09-13 14:10:34 -0700

    my task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet - function to print abc
  *
- *  Return: 0
+ * Return: 0
  */
 void print_alphabet(void)
 {
@@ -12,6 +12,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-_putchar('n\');
+_putchar('\n');
 }
-
